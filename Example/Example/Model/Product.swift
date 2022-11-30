9658e4f8b4da6ba1471e729f0ca2e3b6c7ecdc9b
Prem Pratap Singh <prem.pratap@xparrow.com>
2022-11-30 11:19:06 +0530

    Update example product details view to match with figma design specs. Update workflow to present product details only for test mac book pro product for demo purpose.

--- Example/Example/Model/Product.swift
+++ Example/Example/Model/Product.swift
@@ -8,16 +8,32 @@
 import Foundation
 
 class Product {
-    let id: String = UUID().uuidString
-    let icon: String
-    let title: String
+    let id: String
+    let image: String
+    let landscapeImage: String?
+    let name: String
+    let description: String
     let price: Double
     let rating: Double
     let shop: Shop
 
-    init(icon: String, title: String, price: Double, rating: Double, shop: Shop) {
-        self.icon = icon
-        self.title = title
+    static let macbookProId: String = "tsprd001"
+
+    init(
+        id: String,
+        image: String,
+        landscapeImage: String? = nil,
+        name: String,
+        description: String,
+        price: Double,
+        rating: Double,
+        shop: Shop
+    ) {
+        self.id = id
+        self.image = image
+        self.landscapeImage = landscapeImage
+        self.name = name
+        self.description = description
         self.price = price
         self.rating = rating
         self.shop = shop
@@ -29,15 +45,20 @@ extension Product {
         return [
             [
                 Product(
-                    icon: "appleMacbook",
-                    title: "Macbook Pro (M1)",
+                    id: Product.macbookProId,
+                    image: "appleMacbook",
+                    landscapeImage: "appleMacbookLandscape",
+                    name: "Macbook Pro (M1)",
+                    description: "Protect your device from regular wear and tear by getting the Apple MacBook Pro 14. MacBook Pro was designed to minimize its impact on the environment. It’s free of harmful chemicals and materials. Now, you can use a single keyboard and mouse pad to work seamlessly between your iPad with universal control.",
                     price: 6500,
                     rating: 4.5,
                     shop: Shop.apple
                 ),
                 Product(
-                    icon: "samsungLaptop",
-                    title: "Samsung galaxy book2 (16 inch)",
+                    id: "tsprd002",
+                    image: "samsungLaptop",
+                    name: "Samsung galaxy book2 (16 inch)",
+                    description: "",
                     price: 4500,
                     rating: 3.5,
                     shop: Shop.samsung
@@ -45,15 +66,19 @@ extension Product {
             ],
             [
                 Product(
-                    icon: "sonyLaptop",
-                    title: "Sony vio",
+                    id: "tsprd003",
+                    image: "sonyLaptop",
+                    name: "Sony vio",
+                    description: "",
                     price: 4200,
-                    rating: 4.0,
+                    rating: 3.0,
                     shop: Shop.sony
                 ),
                 Product(
-                    icon: "appleMacbookAir",
-                    title: "Apple macbook air",
+                    id: "tsprd004",
+                    image: "appleMacbookAir",
+                    name: "Apple macbook air",
+                    description: "",
                     price: 3800,
                     rating: 4.5,
                     shop: Shop.apple
@@ -61,17 +86,21 @@ extension Product {
             ],
             [
                 Product(
-                    icon: "appleMacbook",
-                    title: "Macbook Pro (M2)",
+                    id: "tsprd005",
+                    image: "appleMacbook",
+                    name: "Macbook Pro (M2)",
+                    description: "",
                     price: 10500,
-                    rating: 5.0,
+                    rating: 4.0,
                     shop: Shop.apple
                 ),
                 Product(
-                    icon: "samsungLaptop",
-                    title: "Samsung galaxy book2 (13 inch)",
+                    id: "tsprd006",
+                    image: "samsungLaptop",
+                    name: "Samsung galaxy book2 (13 inch)",
+                    description: "",
                     price: 3500,
-                    rating: 4.0,
+                    rating: 3.0,
                     shop: Shop.samsung
                 )
             ]
--- Example/Example/View/ProductsListView.swift
+++ Example/Example/View/ProductsListView.swift
@@ -14,112 +14,142 @@ struct ProductsListView: View {
 
     @State private var productWidgetWidth: CGFloat = 0
     @State private var isTrustbadgeVisible: Bool = true
+    @State private var selectedProduct: Product?
+    @State private var shouldShowProductDetails: Bool = false
 
     private var productWidgetSpacing: CGFloat = 12
     private var horizontalPadding: CGFloat = 16
 
     var body: some View {
-        ZStack {
-            // Background
-            Color.tsOffWhite
-                .ignoresSafeArea()
+        NavigationView {
+            ZStack {
+                // Background
+                Color.tsOffWhite
+                    .ignoresSafeArea()
 
-            // Content view
-            VStack {
+                // Content view
+                VStack {
 
-                // Title text
-                Text("Products")
-                    .font(.system(size: 16, weight: .semibold))
-                    .foregroundColor(Color.black)
-                    .padding(.top, 80)
+                    // Title text
+                    Text("Products")
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundColor(Color.black)
+                        .padding(.top, 80)
 
-                // Toolbar
-                HStack(alignment: .center) {
-                    Image("gridLayoutIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40, height: 40)
-                    Spacer()
-                    Image("sortButtonIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 40)
-                    Spacer()
-                    Image("filterButtonIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 40)
-                }
-                .padding(.top, 20)
+                    // Toolbar
+                    HStack(alignment: .center) {
+                        Image("gridLayoutIcon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                        Spacer()
+                        Image("sortButtonIcon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 40)
+                        Spacer()
+                        Image("filterButtonIcon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 40)
+                    }
+                    .padding(.top, 20)
 
-                // Product categories section
-                VStack(alignment: .leading) {
-                    Text("Available \(Product.testElectronicsProducts.count * 2) products")
-                        .font(.system(size: 16, weight: .regular))
-                        .foregroundColor(Color.tsGray600)
-                    Rectangle()
-                        .fill(Color.tsGray300)
-                        .frame(height: 1)
-                }
-                .padding(.top, 20)
+                    // Product categories section
+                    VStack(alignment: .leading) {
+                        Text("Available \(Product.testElectronicsProducts.count * 2) products")
+                            .font(.system(size: 16, weight: .regular))
+                            .foregroundColor(Color.tsGray600)
+                        Rectangle()
+                            .fill(Color.tsGray300)
+                            .frame(height: 1)
+                    }
+                    .padding(.top, 20)
 
 
-                // Product categories
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: self.productWidgetSpacing) {
-                        ForEach(0..<Product.testElectronicsProducts.count) { i in
-                            let products: [Product] = Product.testElectronicsProducts[i]
-                            HStack(alignment: .top, spacing: self.productWidgetSpacing) {
-                                ForEach(0..<products.count) { j in
-                                    let product = products[j]
-                                    ProductDetailsView(
-                                        product: product,
-                                        width: self.productWidgetWidth
-                                    )
-                                    .onTapGesture {
-                                        //self.showProductsForTheCategory(productCategory)
+                    // Product categories
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(spacing: self.productWidgetSpacing) {
+                            ForEach(0..<Product.testElectronicsProducts.count) { i in
+                                let products: [Product] = Product.testElectronicsProducts[i]
+                                HStack(alignment: .top, spacing: self.productWidgetSpacing) {
+                                    ForEach(0..<products.count) { j in
+                                        let product = products[j]
+                                        ProductWidgetView(
+                                            product: product,
+                                            width: self.productWidgetWidth
+                                        )
+                                        .onTapGesture {
+                                            self.showDetailsForProduct(product)
+                                        }
                                     }
                                 }
                             }
-                        }
 
-                        GeometryReader { proxy in
-                            let offset = proxy.frame(in: .named("scroll")).minY
-                            Color.clear.preference(key: ScrollViewOffsetPreferenceKey.self, value: offset)
+                            GeometryReader { proxy in
+                                let offset = proxy.frame(in: .named("scroll")).minY
+                                Color.clear.preference(key: ScrollViewOffsetPreferenceKey.self, value: offset)
+                            }
                         }
+                        .padding(.bottom, 140)
                     }
-                    .padding(.bottom, 140)
+                    .padding(.top, 20)
+                    .coordinateSpace(name: "scroll")
+                    .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+                        let targetScrollOffset: CGFloat = 750
+                        self.isTrustbadgeVisible = value >= targetScrollOffset
+                    }
                 }
-                .padding(.top, 20)
-                .coordinateSpace(name: "scroll")
-                .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
-                    let targetScrollOffset: CGFloat = 750
-                    self.isTrustbadgeVisible = value >= targetScrollOffset
-                }
-            }
-            .padding(.horizontal, 16)
+                .padding(.horizontal, 16)
 
-            // Trustbadge and it's container view
-            VStack {
-                Spacer()
-                TrustbadgeView(tsid: "X330A2E7D449E31E467D2F53A55DDD070", context: .productGrade)
-                    .frame(width: 100, height: 100)
-                    .opacity(self.isTrustbadgeVisible ? 1 : 0)
-                    .animation(.easeOut(duration: 0.3))
+                // Trustbadge and it's container view
+                VStack {
+                    Spacer()
+                    TrustbadgeView(tsid: "X330A2E7D449E31E467D2F53A55DDD070", context: .trustMark)
+                        .frame(width: 100, height: 100)
+                        .opacity(self.isTrustbadgeVisible ? 1 : 0)
+                        .animation(.easeOut(duration: 0.3))
+                }
+                .padding(.bottom, 110)
+
+                // Link to product details view
+                if let product = self.selectedProduct {
+                    NavigationLink(
+                        destination: ProductDetailsView(
+                            productDetails: product
+                        ),
+                        isActive: self.$shouldShowProductDetails) {
+                            EmptyView()
+                        }
+                }
             }
-            .padding(.bottom, 110)
+            .navigationBarHidden(true)
+            .ignoresSafeArea(.all)
+            .onAppear {
+                self.productWidgetWidth = (UIScreen.main.bounds.width - 2*self.horizontalPadding - self.productWidgetSpacing) * 0.5
+            }
         }
-        .ignoresSafeArea(.all)
-        .onAppear {
-            self.productWidgetWidth = (UIScreen.main.bounds.width - 2*self.horizontalPadding - self.productWidgetSpacing) * 0.5
-        }
+        .navigationBarHidden(true)
+    }
+
+    // MARK: Private methods
+
+    /**
+     Presents product details view to show details about the selected product
+     */
+    private func showDetailsForProduct(_ product: Product) {
+        //For the demo purpose, we show product details only for Apple macbook pro
+        guard product.id == Product.macbookProId else { return }
+
+        self.selectedProduct = product
+        self.shouldShowProductDetails = true
     }
 }
 
 /**
  ProductDetailsView shows details about a product like icon, name, price, brand etc
  */
-struct ProductDetailsView: View {
+struct ProductWidgetView: View {
 
     // MARK: Public properties
 
@@ -130,10 +160,10 @@ struct ProductDetailsView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Image(self.product.icon)
+            Image(self.product.image)
                 .resizable()
                 .scaledToFit()
-            Text(self.product.title)
+            Text(self.product.name)
                 .font(.system(size: 14, weight: .regular))
                 .foregroundColor(Color.black)
                 .padding(.leading, 10)
@@ -154,7 +184,6 @@ struct ProductDetailsView: View {
                 Text("\(self.product.price.formatted()) €")
                     .font(.system(size: 12, weight: .regular))
                     .foregroundColor(Color.black)
-
 
             }
             .padding(.leading, 10)
