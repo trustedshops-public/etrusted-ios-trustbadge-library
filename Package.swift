ebda6561aa8f9bc5059ef14b4cb7da9c887417fe
Prem Pratap Singh <prem.pratap@xparrow.com>
2023-01-28 18:55:58 +0530

    Add swift package specification

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Trustylib",
+defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Trustylib",
+            targets: ["Trustylib"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Trustylib",
+path: "Trustylib/Trustylib"
+        )
+    ]
+)
