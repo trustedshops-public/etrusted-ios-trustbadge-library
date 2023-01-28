0540c7ac195f77b8308c47ecb987c3caecfe822f
Prem Pratap Singh <prem.pratap@xparrow.com>
2023-01-28 23:32:23 +0530

    Add swift package specification

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Trustylib",
 defaultLocalization: "en",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
