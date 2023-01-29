c9eb4f3d0345a3c967d09a341df266d7a7de897e
Prem Pratap Singh <prem.pratap@xparrow.com>
2023-01-29 11:34:27 +0530

    Update swift package specification

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Trustylib",
-            path: "Trustylib/Trustylib"
+            path: "Trustylib/Trustylib",
+            resources: ["Trustylib/Trustylib/*.xcassets"]
         )
     ]
 )
