b410c4a9ff18d06d4db3dcefcf9c21775aea9507
Prem Pratap Singh <prem.pratap@icloud.com>
2023-03-01 14:15:19 +0530

    Add back unit tests which were removed after failing trustmark details API
    Signed-off-by: Prem Pratap Singh <prem.pratap@icloud.com>

--- Tests/TrustylibTests/TrustmarkDataServiceTests.swift
+++ Tests/TrustylibTests/TrustmarkDataServiceTests.swift
@@ -14,23 +14,35 @@ import XCTest
  */
 
 final class TrustmarkDataServiceTests: XCTestCase {
-
+    
     private let trustmarkDataService = TrustmarkDataService()
     private var didLoadTrustmarkDetails = false
-
+    
     override func setUpWithError() throws {
         self.didLoadTrustmarkDetails = false
     }
-
+    
     func testTrustmarkDataServiceReturnsNilForInvalidTSID() throws {
         let trustmarkExpectation = expectation(description: "TrustmarkDataService response expectation")
         self.trustmarkDataService.getTrustmarkDetails(for: "testTSID") { details in
             self.didLoadTrustmarkDetails = details != nil
             trustmarkExpectation.fulfill()
         }
-
+        
         waitForExpectations(timeout: 5)
         XCTAssertFalse(self.didLoadTrustmarkDetails,
                        "TrustmarkDataService should return nil value for an invalid TSID")
+    }
+    
+    func testTrustmarkDataServiceReturnsDetailsForValidTSID() throws {
+        let trustmarkExpectation = expectation(description: "TrustmarkDataService response expectation")
+        self.trustmarkDataService.getTrustmarkDetails(for: "X330A2E7D449E31E467D2F53A55DDD070") { details in
+            self.didLoadTrustmarkDetails = details != nil
+            trustmarkExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+        XCTAssertTrue(self.didLoadTrustmarkDetails,
+                      "TrustmarkDataService should return trustmark details for a valid TSID")
     }
 }
