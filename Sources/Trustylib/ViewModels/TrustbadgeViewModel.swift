d65690ccb807121f6de6eaf37c0145743bb0ac97
Prem Pratap Singh <prem.pratap@icloud.com>
2023-06-20 03:22:26 +0530

    Add workflow to set trustbadge widget's color scheme based on the client app's color scheme related configurations.
    Signed-off-by: Prem Pratap Singh <prem.pratap@icloud.com>

--- Sources/Trustylib/ViewModels/TrustbadgeViewModel.swift
+++ Sources/Trustylib/ViewModels/TrustbadgeViewModel.swift
@@ -47,6 +47,11 @@ class TrustbadgeViewModel: ObservableObject {
     var productId: String?
     var context: TrustbadgeContext
     var alignment: TrustbadgeViewAlignment = .leading
+    var colorScheme: TrustbadgeColorScheme = .system {
+        didSet {
+            self.setIconForState()
+        }
+    }
     
     /**
      It validates, if valid inputs are provided as required for the widget to fetch data from backend and render
@@ -163,6 +168,11 @@ class TrustbadgeViewModel: ObservableObject {
      */
     func setIconForState() {
         if self.currentState == .default(self.isTrustmarkValid) {
+            if self.isTrustmarkValid {
+                self.iconImageName = "trustMarkIcon"
+            } else {
+                self.iconImageName = self.colorScheme.iconImageName
+            }
             self.iconImageName = self.currentState.iconImageName
         } else if self.currentState == .expended {
             if self.context == .productGrade, let productImage = self.productImage {
---
+++ Tests/TrustylibTests/TrustbadeColorSchemeManagerTests.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright (C) 2023 Trusted Shops AG
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//  Created by Prem Pratap Singh on 20/06/23.
+//
+
+
+import XCTest
+import SwiftUI
+@testable import Trustylib
+
+/**
+ TrustbadeColorSchemeManagerTests tests the TrustbadeColorSchemeManager workflows for
+ updating library widgets' color and assets
+ */
+final class TrustbadeColorSchemeManagerTests: XCTestCase {
+    
+    func testTrustbadeColorSchemeManagerUpdatesColorsBasedOnActiveColorScheme() throws {
+        let manager = TrustbadgeColorSchemeManager.instance
+        manager.updateColorsForScheme(.dark)
+        XCTAssertTrue(
+            manager.borderColor.description == Color.tsGray700.description,
+            "TrustbadgeColorSchemeManager should update color codes based on active color scheme"
+        )
+        XCTAssertTrue(
+            manager.backgroundColor.description == Color.tsGray800.description,
+            "TrustbadgeColorSchemeManager should update color codes based on active color scheme"
+        )
+        XCTAssertTrue(
+            manager.titleTextColor.description == Color.white.description,
+            "TrustbadgeColorSchemeManager should update color codes based on active color scheme"
+        )
+        XCTAssertTrue(
+            manager.ratingTextColor.description == Color.white.description,
+            "TrustbadgeColorSchemeManager should update color codes based on active color scheme"
+        )
+        XCTAssertTrue(
+            manager.ratingRangeTextColor.description == Color.white.description,
+            "TrustbadgeColorSchemeManager should update color codes based on active color scheme"
+        )
+    }
+
+}
